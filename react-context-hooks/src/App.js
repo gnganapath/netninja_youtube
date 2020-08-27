2b807ae7be4bcfa7d10ee0d08f8385d52b7ab30c
gnganapath <gnganapathi@gmail.com>
2020-08-27 21:12:02 +0530

    plain projects for hooks, redux to begin

---
+++ react-context-hooks/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      plain - react context- hooks (functional) component app
+	  
+    </div>
+  );
+}
+
+export default App;
---
+++ react-redux-firebase-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      plain - react redux functional component app
+    </div>
+  );
+}
+
+export default App;
